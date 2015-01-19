48bff991399936dbb7ad89ba454a8ff8bd63a347
Alejandro Jurado García-Roco <alexthe21@gmail.com>
2015-01-19 22:42:21 +0000

    Redirect Logic

--- src/At21/EBoxOfficeBundle/EventListener/SecurityListener.php
+++ src/At21/EBoxOfficeBundle/EventListener/SecurityListener.php
@@ -9,27 +9,32 @@
 namespace At21\EBoxOfficeBundle\EventListener;
 
 
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Debug\TraceableEventDispatcher;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Routing\Router;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SecurityListener
 {
+    protected $session;
     protected $router;
     protected $security;
     protected $dispatcher;
 
-    public function __construct(Router $router, SecurityContext $security, TraceableEventDispatcher $dispatcher)
+    public function __construct(
+        Session $session,
+        UrlGeneratorInterface $router,
+        AuthorizationChecker $authorizationChecker,
+        TraceableEventDispatcher $dispatcher
+    )
     {
+        $this->session = $session;
         $this->router = $router;
-        $this->security = $security;
+        $this->authorizationChecker = $authorizationChecker;
         $this->dispatcher = $dispatcher;
     }
 
@@ -40,9 +45,9 @@ class SecurityListener
 
     public function onKernelResponse(FilterResponseEvent $event)
     {
-        if ($this->security->isGranted('ROLE_ADMIN')) {
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
             $response = new RedirectResponse($this->router->generate('at21_eboxoffice_admin'));
-        } elseif ($this->security->isGranted('ROLE_USER')) {
+        } elseif ($this->authorizationChecker->isGranted('ROLE_USER')) {
             $response = new RedirectResponse($this->router->generate('at21_eboxoffice_user'));
         } else {
             $response = new RedirectResponse($this->router->generate('at21_eboxoffice_homepage'));
