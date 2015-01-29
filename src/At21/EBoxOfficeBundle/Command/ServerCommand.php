c5d9c66aaa4cad3858ec53757483a2081f20bfe1
Alejandro Jurado García-Roco <alexthe21@gmail.com>
2015-01-29 18:02:18 +0000

    Socket Not Working

---
+++ src/At21/EBoxOfficeBundle/Command/ServerCommand.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 29/01/15
+ * Time: 16:21
+ */
+
+namespace At21\EBoxOfficeBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Ratchet\Server\IoServer;
+
+class ServerCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('at21:eboxoffice:server')
+            ->setDescription('Start the BoxOffice server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $chat = $this->getContainer()->get('at21_eboxoffice_boxoffice');
+        $port = 8080;
+        $server = IoServer::factory($chat, $port);
+        $output->writeln('');
+        $output->writeln('E-BoxOffice Running and listening *:' . $port);
+        $output->writeln('');
+        $output->writeln('Quit the server with CONTROL + C');
+        $server->run();
+    }
+}
